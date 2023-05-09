009f93c2f79526c319dd179d6933bdba950d6f7a
darjeeling4g <darjeeling4g@gmail.com>
2023-05-09 16:22:24 +0900

    [inOneWeekend]: Refatoring: vec3.h

--- Graphics/RT_in_one_weekend/vec3.h
+++ Graphics/RT_in_one_weekend/vec3.h
@@ -8,9 +8,8 @@ using std::sqrt;
 
 class vec3 {
     public:
-        vec3() : e{0,0,0} {}
-        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
-
+        vec3() { e[0] = 0, e[1] = 0, e[2] = 0; }
+        vec3(double e0, double e1, double e2) { e[0] = e0, e[1] = e1, e[2] = e2; }
         double x() const { return e[0]; }
         double y() const { return e[1]; }
         double z() const { return e[2]; }
@@ -53,7 +52,6 @@ class vec3 {
 using point3 = vec3;   // 3D point
 using color = vec3;    // RGB color
 
-#endif
 
 // vec3 Utility Functions
 
@@ -100,3 +98,5 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
+
+#endif
